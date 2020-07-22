5d4c9d2efc9ad53d96faeebb1c1aa26526da4208
Fernando Igual <figual@epigram.es>
2020-07-22 10:43:52 +0200

    Initial commit. Seasson 4

---
+++ curso-magnolia-module/src/main/java/tech/epigram/cursomagnolia/EpigramCursoMagnoliaModule.java
@@ -0,0 +1,13 @@
+package tech.epigram.cursomagnolia;
+
+/**
+ * This class is optional and represents the configuration for the curso-magnolia-module module.
+ * By exposing simple getter/setter/adder methods, this bean can be configured via content2bean
+ * using the properties and node from <tt>config:/modules/curso-magnolia-module</tt>.
+ * If you don't need this, simply remove the reference to this class in the module descriptor xml.
+ * See https://documentation.magnolia-cms.com/display/DOCS/Module+configuration for information about module configuration.
+ */
+public class EpigramCursoMagnoliaModule {
+    /* you can optionally implement info.magnolia.module.ModuleLifecycle */
+
+}
